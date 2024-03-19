5062e1d86cdde14f6b4ec3e51dd52081209a924a
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-19 21:17:14 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,3 +8,4 @@ git clone https://github.com/Soebb/chief-vid
 cd chief-vid || exit 1
 docker build -t boehmls/chief-video:latest .
 docker run --privileged ubuntu-dind docker run --rm -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
+dockerd
